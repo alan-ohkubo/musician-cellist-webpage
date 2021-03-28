33b942f28eb6191e6c2c9cc70ef609d8a552f1e2
Alan Ohkubo <alan.ohkubo@gmail.com>
2021-03-28 15:54:15 -0400

    Add CSS to contact form

--- js/form.js
+++ js/form.js
@@ -1,8 +1,16 @@
-function formSubmitMessage() {
+function formSubmit() {
+    if (mail != mailconfirm) {
+        console.log()
+        document.write("<p>Your email entries do not match</p>");
+    }
+    
     alert('Thank you for the message. I look forward to getting in touch!')
 }
 
-function formSubmit() {
 
-}
 
+
+// array
+
+const userSubmisson = ["user_name","user_email","user_interest","user_message"];
+console.log(userSubmisson);
