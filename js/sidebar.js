2630cc0c5a7a8abcc2a1214944772c68d097f4ba
Alan Ohkubo <alan.ohkubo@gmail.com>
2021-03-27 22:28:00 -0400

    Add media querie for side nav bar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,11 @@
 
+var mq = window.matchMedia('@media (max-width:767px)')
+
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("sideNav").style.width = "50%";
-    document.getElementById("main").style.marginLeft = "50%";
+    document.getElementById("sideNav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "auto";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
