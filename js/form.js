6cff9b8c3bd227f8822b867457cf8d381fcf874c
Alan Ohkubo <alan.ohkubo@gmail.com>
2021-03-28 11:40:58 -0400

    Abandon the image carousel

--- js/form.js
+++ js/form.js
@@ -8,18 +8,25 @@ function formSubmit() {
 
 // SLIDESHOW
 
-var slideIndex = 0;
-showSlides();
+document.addEventListener( 'DOMContentLoaded', function() {} );
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); 
-    // Change image every 3000ms = 3 seconds
-}
+
+var slide_index = 1;  
+displaySlides(slide_index);  
+function nextSlide(n) {  
+    displaySlides(slide_index += n);  
+}  
+function currentSlide(n) {  
+    displaySlides(slide_index = n);  
+}  
+function displaySlides(n) {  
+    var i;  
+    var slides = document.getElementsByClassName("showSlide");  
+    if (n > slides.length) { slide_index = 1 }  
+    if (n < 1) { slide_index = slides.length }  
+    for (i = 0; i < slides.length; i++) {  
+        slides[i].style.display = "none";  
+    }  
+    slides[slide_index - 1].style.display = "block";  
+}  
+             
