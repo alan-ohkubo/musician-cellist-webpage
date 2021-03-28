caa92abd10ca6fa7ab0dad8627ead87dd0707df5
Alan Ohkubo <alan.ohkubo@gmail.com>
2021-03-27 23:28:36 -0400

    Adjust media queries

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,5 @@
 
-var mq = window.matchMedia('@media (max-width:767px)')
+// var mq = window.matchMedia('@media (max-width:767px)')
 
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
